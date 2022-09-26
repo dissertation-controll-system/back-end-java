1150c50270dab8bc18c6419b2d2485b21f7e1128
AntonMishyn <mishinaa333@gmail.com>
2022-09-25 20:57:16 -0400

    Migration

--- src/main/resources/changelog/release-1.0/migrations/schema-1.sql
+++ src/main/resources/changelog/release-1.0/migrations/schema-1.sql
@@ -1,92 +1,80 @@
-create table account
-(
-    account_id              bigserial           PRIMARY KEY,
-    created_at              timestamp           DEFAULT now(),
-    created_by              varchar(255)        DEFAULT 'system',
-    modified_at             timestamp           DEFAULT now(),
-    modified_by             varchar(255)        DEFAULT 'system',
-    active                  boolean             DEFAULT true NOT NULL,
-    email                   varchar(255)        NOT NULL,
-    email_activation_state  integer             DEFAULT 0    NOT NULL,
-    password                varchar(255)        NOT NULL,
-    username                varchar(255)        NOT NULL
-        constraint uk_gex1lmaqpg0ir5g1f5eftyaa1
-            unique,
-    user_id                bigint
-);
-
 create table faculty
 (
-    faculty_id  bigserial PRIMARY KEY,
-    created_at  timestamp DEFAULT now(),
-    created_by  varchar(255) DEFAULT 'system',
-    modified_at timestamp DEFAULT now(),
-    modified_by varchar(255) DEFAULT 'system',
+    faculty_id  bigserial primary key,
+    created_at  timestamp default now(),
+    created_by  varchar(255) default 'system',
+    modified_at timestamp default now(),
+    modified_by varchar(255) default 'system',
     name        varchar(255)
+);
+
+create table users
+(
+    user_id      bigserial primary key,
+    created_at   timestamp default now(),
+    created_by   varchar(255) default 'system',
+    modified_at  timestamp default now(),
+    modified_by  varchar(255) default 'system',
+    fathers_name varchar(255),
+    first_name   varchar(255),
+    last_name    varchar(255),
+    type         varchar(255) not null
 );
 
 create table cathedra
 (
-    cathedra_id bigserial
-        primary key,
-    created_at  timestamp DEFAULT now(),
-    created_by  varchar(255) DEFAULT 'system',
-    modified_at timestamp DEFAULT now(),
-    modified_by varchar(255) DEFAULT 'system',
+    cathedra_id bigserial primary key,
+    created_at  timestamp default now(),
+    created_by  varchar(255) default 'system',
+    modified_at timestamp default now(),
+    modified_by varchar(255) default 'system',
     name        varchar(255),
     faculty_id  bigint not null
-        constraint fkdda292n339cqonlqlgmlg0han
-            references faculty
+        constraint fk_cathedra_faculty references faculty
+);
+
+create table user_cathedra
+(
+    user_id     bigint not null
+        constraint fk_user_cathedra_users references users,
+    cathedra_id bigint not null
+        constraint fk_user_cathedra_cathedra references cathedra,
+    primary key (user_id, cathedra_id)
+);
+
+create table account
+(
+    account_id             bigserial primary key,
+    created_at             timestamp default now(),
+    created_by             varchar(255)  default 'system',
+    modified_at            timestamp default now(),
+    modified_by            varchar(255) default 'system',
+    active                 boolean default true not null,
+    email                  varchar(255)         not null,
+    email_activation_state integer default 0    not null,
+    password               varchar(255)         not null,
+    username               varchar(255)         not null
+        constraint uk_account_username unique,
+    user_id                bigint
+        constraint uk_account_user_id unique
+        constraint fk_account_users references users
 );
 
 create table role
 (
-    role_id     bigserial
-        primary key,
-    created_at  timestamp DEFAULT now(),
-    created_by  varchar(255) DEFAULT 'system',
-    modified_at timestamp DEFAULT now(),
-    modified_by varchar(255) DEFAULT 'system',
+    role_id     bigserial primary key,
+    created_at  timestamp default now(),
+    created_by  varchar(255) default 'system',
+    modified_at timestamp default now(),
+    modified_by varchar(255) default 'system',
     name        varchar(255)
 );
 
 create table account_role
 (
     account_id bigint not null
-        constraint fk1f8y4iy71kb1arff79s71j0dh
-            references account,
+        constraint fk_account_role_account references account,
     role_id    bigint not null
-        constraint fkrs2s3m3039h0xt8d5yhwbuyam
-            references role
+        constraint fk_account_role_role references role,
+    primary key (account_id, role_id)
 );
-
-create table users
-(
-    user_id         bigserial  primary key,
-    created_at      timestamp DEFAULT now(),
-    created_by      varchar(255) DEFAULT 'system',
-    modified_at     timestamp DEFAULT now(),
-    modified_by     varchar(255) DEFAULT 'system',
-    fathers_name    varchar(255),
-    first_name      varchar(255),
-    last_name       varchar(255),
-    type            varchar(255) not null,
-    account_id   bigint
-        constraint fk3pwaj86pwopu3ot96qlrfo2up
-            references account
-);
-
-alter table account
-    add constraint fkra7xoi9wtlcq07tmoxxe5jrh4
-        foreign key (user_id) references users;
-
-create table user_cathedra
-(
-    user_id     bigint not null
-        constraint fkf1qysfvge2olhj64dnv5fh2ny
-            references users,
-    cathedra_id bigint not null
-        constraint fkhp9i0yy41y2pws59onv33o6l6
-            references cathedra
-);
-
